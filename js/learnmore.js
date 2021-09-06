d37c6e9ad20b544825093422a19b2e688c8d5141
jth55 <jthince55@gmail.com>
2021-09-05 19:44:35 -0500

    Week 1 update

---
+++ js/learnmore.js
@@ -0,0 +1,9 @@
+function monLearnMore() {
+	alert("No meeting this week!");
+}
+function tuesLearnMore() {
+	alert("How to use command line interfaces. Don't know anything? Start here! Want to learn some cool terminal tricks, or want a command line refresher? You're invited too!");
+}
+function wedLearnMore() {
+	alert("Come learn about version control with Git and Github. Version control is useful in programming for saving progress, branching timelines, and allowing multiple people to work on a project simultaneously.");
+}
